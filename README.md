c4943fdabd325887aa8ea39c4c686525719a9f59
mori <ys0514@yeah.net>
2025-03-10 17:55:46 +0800

    test

---
+++ README.md
@@ -0,0 +1,8 @@
+# Mori: Vite + React
+
+-   Vite
+-   React
+-   Less
+-   TypeScript
+-   ESLint
+-   React-Scan (Automatically remove in production environment)
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { BlogCard } from "mori-components";
+import "./index.less";
+const App = () => {
+    return (
+        <div className="mt-4">
+            <BlogCard tagIcons={["AppleDark"]} />
+        </div>
+    );
+};
+
+export default App;
