7ef399a577d69f4992991f3fcee34e1c18cf1fb8
mori <ys0514@yeah.net>
2025-03-19 15:19:32 +0800

    upadate:submodule

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./index.less";
 const App = () => {
     return (
         <div className="mt-4">
-            <BlogCard tagIcons={["AppleDark"]} />
+            <BlogCard title="Title" time="x-xx" decs="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo saepe, in quibusdam alias facilis officiis ad non animi consectetur officia tenetur molestias doloribus ratione eaque! Commodi voluptatem aut inventore quo?" tagIcons={["AppleDark"]} />
         </div>
     );
 };
